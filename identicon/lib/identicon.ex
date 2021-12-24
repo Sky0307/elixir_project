51bfd3eef2147d976c1be07cb325b9ba675ca138
SkyLee <jetxuen_lee@mymail.sutd.edu.sg>
2021-12-24 11:05:12 +0800

    pick color done

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,6 +6,14 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do # taking a variable image, then pattern match it
+    #%Identicon.Image{hex: [r, g, b | _tail]} = image # power of pattern matching
+    # [r, g, b | _tail] = hex_list # ignore the rest of the list after the third element
+
+    %Identicon.Image{image | color: {r, g, b}} # create a new map with updated data
   end
 
   def hash_input(input) do
