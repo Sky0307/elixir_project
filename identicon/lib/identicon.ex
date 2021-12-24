3dde0023442d2969870e97fd85d826f20196ec86
SkyLee <jetxuen_lee@mymail.sutd.edu.sg>
2021-12-24 17:14:31 +0800

    fix chunk deprecated

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -9,6 +9,21 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
@@ -24,7 +39,8 @@ defmodule Identicon do
   def build_grid(%Identicon.Image{hex: hex} = image) do
     grid =
       hex
-      |> Enum.chunk(3)
+      |> Enum.chunk_every(3, 3, :discard)
+      |> IO.inspect
       |> Enum.map(&mirror_row/1)
       |> List.flatten
       |> Enum.with_index
